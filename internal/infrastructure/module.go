a8fd9d17a53c32c4ac9fe7a55532f78a28e7f017
Ali127Hub <AliMoradi0Business@gmail.com>
2026-04-26 10:03:19 +0330

    feat: implement clock contract

--- internal/infrastructure/module.go
+++ internal/infrastructure/module.go
@@ -2,6 +2,7 @@ package infrastructure
 
 import (
 	"auth/internal/domain/contract"
+	"auth/internal/infrastructure/clock"
 	"auth/internal/infrastructure/uuid"
 
 	"go.uber.org/fx"
@@ -13,6 +14,10 @@ var Module = fx.Module(
 		fx.Annotate(
 			uuid.NewKsuidIdGenerator,
 			fx.As(new(contract.UuidGenerator)),
+		),
+		fx.Annotate(
+			clock.NewSystemClock,
+			fx.As(new(contract.Clock)),
 		),
 	),
 )
